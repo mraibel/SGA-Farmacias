878c08648c5b1c4463e3ded2ef9d66db8677301d
mraibel <mraibel12@gmail.com>
2025-05-15 02:30:32 -0400

    configuraciones api gateway

--- microservicios/usuario-servicio/src/main/java/com/example/usuario_servicio/UsuarioServicioApplication.java
+++ microservicios/usuario-servicio/src/main/java/com/example/usuario_servicio/UsuarioServicioApplication.java
@@ -2,9 +2,12 @@ package com.example.usuario_servicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 public class UsuarioServicioApplication {
