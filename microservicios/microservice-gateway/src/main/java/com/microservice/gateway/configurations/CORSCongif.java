875b566742b110a6cefb7f68bd48ddb061934f4a
mraibel <mraibel12@gmail.com>
2025-05-31 20:57:52 -0400

    cors gateway

---
+++ microservicios/microservice-gateway/src/main/java/com/microservice/gateway/configurations/CORSCongif.java
@@ -0,0 +1,27 @@
+package com.microservice.gateway.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class CORSCongif implements WebFluxConfigurer {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("*")); // Cambia por tu dominio si es necesario
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
