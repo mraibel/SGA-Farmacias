d5975da8aeab8a7ca4985fcfd7c1056486080fb5
FelidevX <felipe2002ft@gmail.com>
2025-06-01 00:08:48 -0400

    Config gateway + eureka

--- microservicios/inventario-servicio/src/main/java/com/example/inventario_servicio/InventarioServicioApplication.java
+++ microservicios/inventario-servicio/src/main/java/com/example/inventario_servicio/InventarioServicioApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 public class InventarioServicioApplication {
