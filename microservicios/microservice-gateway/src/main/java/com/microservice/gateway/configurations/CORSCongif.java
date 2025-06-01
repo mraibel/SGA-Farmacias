548b67206b2dd2303f48f13b7fdda6c8dc52ae50
mraibel <mraibel12@gmail.com>
2025-05-31 22:05:03 -0400

    cors gateway

--- microservicios/microservice-gateway/src/main/java/com/microservice/gateway/configurations/CORSCongif.java
+++ microservicios/microservice-gateway/src/main/java/com/microservice/gateway/configurations/CORSCongif.java
@@ -3,7 +3,7 @@ package com.microservice.gateway.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
@@ -13,15 +13,16 @@ import java.util.List;
 public class CORSCongif implements WebFluxConfigurer {
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("*")); // Cambia por tu dominio si es necesario
+        config.setAllowedOrigins(List.of("*")); // O reemplaza "*" por tu origen exacto
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-        return source;
+
+        return new CorsWebFilter(source);
     }
 }
