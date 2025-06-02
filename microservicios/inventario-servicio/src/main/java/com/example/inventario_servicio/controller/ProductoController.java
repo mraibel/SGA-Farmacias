55bb1269aa0b1390d7d1ced42a7367f4c2971c93
mraibel <mraibel12@gmail.com>
2025-06-02 09:00:17 -0400

    prueba

--- microservicios/inventario-servicio/src/main/java/com/example/inventario_servicio/controller/ProductoController.java
+++ microservicios/inventario-servicio/src/main/java/com/example/inventario_servicio/controller/ProductoController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/productosss")
+@RequestMapping("/productos")
 public class ProductoController {
 
     private final ProductoRepository productoRepository;
