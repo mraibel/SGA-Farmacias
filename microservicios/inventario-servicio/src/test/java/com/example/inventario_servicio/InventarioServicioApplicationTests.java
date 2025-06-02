b843411bb0b5e55d844e02ed9bb8b61c24b04009
mraibel <mraibel12@gmail.com>
2025-06-01 21:17:30 -0400

    fix build

--- microservicios/inventario-servicio/src/test/java/com/example/inventario_servicio/InventarioServicioApplicationTests.java
+++ microservicios/inventario-servicio/src/test/java/com/example/inventario_servicio/InventarioServicioApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.inventario_servicio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InventarioServicioApplicationTests {
 
 	@Test
