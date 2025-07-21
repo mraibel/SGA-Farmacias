d63f25fc2fa670ccd1a918c22c0916ce7eba7f06
mraibel <mraibel12@gmail.com>
2025-07-20 23:43:24 -0400

    dep ventas

--- microservicios/ventas-servicio/src/test/java/com/microservice/ventas/Microservicio/Ventas/MicroservicioVentasApplicationTests.java
+++ microservicios/ventas-servicio/src/test/java/com/microservice/ventas/Microservicio/Ventas/MicroservicioVentasApplicationTests.java
@@ -1,25 +1,8 @@
 package com.microservice.ventas.Microservicio.Ventas;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import static org.mockito.Mockito.when;
-import org.springframework.test.context.ActiveProfiles;
 
-import com.microservice.ventas.Microservicio.Ventas.client.InventarioCliente;
-
-@ActiveProfiles("test")
 @SpringBootTest
 public class MicroservicioVentasApplicationTests {
 
-    @MockBean
-    private InventarioCliente inventarioCliente;
-
-    @Test
-    void contextoCargaCorrectamente() {
-        when(inventarioCliente.obtenerProductosInventario(null)).thenReturn(null); // Replace null with appropriate mock return value
-        when(inventarioCliente.obtenerProductosInventario(null));
-
-        // test real
-    }
 }
